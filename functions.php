55e88917a73109d538257407f636bf5ae8843fcb
Constantine Kiriaze <ckiriaze@gmail.com>
2014-11-05 15:57:39 -0800

    updated tgm plugin and placed within theme functions

--- functions.php
+++ functions.php
@@ -57,5 +57,6 @@ function simple_child_theme_setup() {
     // require_once locate_template( '/functions/theme-ajax.php', true );
     require_once locate_template( '/functions/theme_tinymce.php', true );
     require_once locate_template( '/functions/theme_extras.php', true );
+    require_once locate_template( '/functions/theme_plugins.php', true );
 
 }
