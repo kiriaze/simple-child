8b1f4a6289989fb080aba59edb925ab6b1dcd1f9
Constantine Kiriaze <ckiriaze@gmail.com>
2014-11-05 16:15:02 -0800

    updated theme plugins to work with an updated version of acf pro after initial install

--- functions/theme_plugins.php
+++ functions/theme_plugins.php
@@ -4,9 +4,10 @@ add_action( 'tgmpa_register', 'simple_child_require_plugins' );
 function simple_child_require_plugins() {
  
     $plugins = array(
+    	// acf pro
 	 	array(
 	        'name'          => 'Advanced Custom Fields Pro',
-	        'slug'          => 'acf-pro',
+	        'slug'          => 'advanced-custom-fields-pro',
 	        'source'        => 'git@bitbucket.org:madebygrizzly/acf-pro.git',
 	        'required'      => true,
 	        'external_url'  => 'git@bitbucket.org:madebygrizzly/acf-pro.git', // If set, overrides default API URL and points to an external URL
